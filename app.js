db51fe2fb0646f8eabeafc9679ed7a37d4d87658
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-06-19 08:33:14 -0400

    Update - Hide container when value is NULL

--- app.js
+++ app.js
@@ -7,23 +7,27 @@ const outputContainer = document.getElementById('output');
 
 // Event Listeners Here
 inputElement.addEventListener('input', (e) => {
-  weight = inputElement.toLocaleString('en-US');
-
-  outputContainer.style.visibility = 'visible';
-  kilogramsElement.innerText = parseFloat(
-    e.target.value * 0.453592
-  ).toLocaleString('en-US');
-  ouncesElement.innerText = parseFloat(e.target.value * 16).toLocaleString(
-    'en-US'
-  );
-  gramsElement.innerText = parseFloat(e.target.value * 453.592).toLocaleString(
-    'en-US'
-  );
+  console.log(e.target.value);
+  if (e.target.value === '' || e.target.value === '0') {
+    // If value is null, hide container
+    outputContainer.style.visibility = 'hidden';
+  } else {
+    // Make container visible
+    outputContainer.style.visibility = 'visible';
+    // Perform calculations
+    kilogramsElement.innerText = parseFloat(
+      e.target.value * 0.453592
+    ).toLocaleString('en-US');
+    ouncesElement.innerText = parseFloat(e.target.value * 16).toLocaleString(
+      'en-US'
+    );
+    gramsElement.innerText = parseFloat(
+      e.target.value * 453.592
+    ).toLocaleString('en-US');
+  }
 });
 
 // Hide Container on Startup or load
 window.onload = (e) => {
   outputContainer.style.visibility = 'hidden';
 };
-
-// Javascript File
